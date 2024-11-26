b79dc559cd1149c422f2235857ec410b80df95f7
SelnoxInfotech <SelnoxInfotech>
2024-11-26 16:33:57 +0530

    add next .config file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   images: {
     remotePatterns: [
       {
@@ -17,26 +21,23 @@ module.exports = {
   webpack: (config, { dev, isServer }) => {
     if (!dev) {
       config.devtool = false; // Disable source maps in production
-    }else {
+    } else {
       // Additional production optimizations
       config.optimization = {
         ...config.optimization,
         splitChunks: {
           chunks: 'all', // Split large chunks into smaller parts
           maxSize: 250000, // Reduce the chunk size limit
-
         },
       };
     }
 
     // Handle large files warnings during the build
     config.performance = {
-      hints: false // Set max entrypoint size to 500 KB
+      hints: false, // Suppress performance hints
     };
 
     // Ensure Webpack processes large JavaScript files
-
-
     return config;
   },
-};
+});
